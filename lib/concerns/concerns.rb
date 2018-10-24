d6918d57abafe1c8ac9b1b495bd2fbabec4052f5
Gilbert Torchon <gilbert@noukod.com>
2018-10-24 13:07:25 +0000

    automatically backed up by learn

--- lib/concerns/concerns.rb
+++ lib/concerns/concerns.rb
@@ -6,12 +6,12 @@ module Concerns
       self.all.select{|class_instance| class_instance.name == name}[0]
     end
 
-    # def find_or_create_by_name(name)
-    #   found = self.all.select{|class_instance| class_instance.name == name}
-    #   if found.nil?
-    #     self.create
-    #   end
-    # end
+    def find_or_create_by_name(name)
+      found = self.all.select{|class_instance| class_instance.name == name}
+      if found.nil?
+        self.create(name)
+      end
+    end
 
   end
 end
