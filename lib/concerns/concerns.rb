86e28826b7c636ea2a746cecaffc66a1be76b972
Gilbert Torchon <gilbert@noukod.com>
2018-10-24 13:06:45 +0000

    automatically backed up by learn

--- lib/concerns/concerns.rb
+++ lib/concerns/concerns.rb
@@ -3,7 +3,7 @@ module Concerns
   module Findable
 
     def find_by_name(name)
-      self.all.select{|class_instance| class_instance.name == name}
+      self.all.select{|class_instance| class_instance.name == name}[0]
     end
 
     # def find_or_create_by_name(name)
